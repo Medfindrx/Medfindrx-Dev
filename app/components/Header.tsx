67727d3c90b1b45923e6d4aebc17d96e6c7a1195
vbhatt552 <vbhatt552@gmail.com>
2025-07-17 12:46:17 +0530

    Improved Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,20 @@
 'use client'
 import React from 'react'
-
+import styles from './Header.module.css';
+import Image from 'next/image';
 const Header = () => {
   return (
     <>
-    <div className='boxy'>
-    <h1 className='Motto'>
+    <div className={styles.boxy}>
+    <h1 className={styles.motto}>
         Revolutionizing<br/>
         Pharmacy Access.<br/>
         Simplifying Operations.
     </h1>
-    <p>
+    <p className={styles.desc}>
         Search medicines nearby,manage inventory and serve patients effeciently.
     </p>
+    <Image src="/Rectangle 4.png" alt="Logo" width ={ 600}height = {400} className={styles.img}/>
     </div>
     </>
   )
